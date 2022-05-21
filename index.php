34a8eec4507b56c92535ab16a2f7e23d0107b66a
Evgeny Kariuk <38400347+HR0N@users.noreply.github.com>
2022-05-21 17:06:09 +0300

    c1

--- index.php
+++ index.php
@@ -9,6 +9,27 @@
     <title>Евгений Коваленко - Таргетолог</title>
 </head>
 <body>
-
+<header class="header">
+    <ul id="header-nav" class="header-nav">
+        <li class="navbar-brand"><a href="/"><img src="http://placehold.jp/3d4070/ffffff/63x63.png" alt="logo"></a></li>
+        <li id="menu-item-21" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-21">
+            <a href="https://wp2.anakim.space/" aria-current="page">Кейсы</a>
+        </li>
+        <li id="menu-item-21" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-home menu-item-21">
+            <a href="https://wp2.anakim.space/" aria-current="page">Отзывы</a>
+        </li>
+        <li id="menu-item-21" class="menu-item menu-item-type-custom menu-item-object-custom  menu-item-home menu-item-21">
+            <a href="https://wp2.anakim.space/" aria-current="page">Контакты</a>
+        </li>
+        <li id="menu-item-24" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-24">
+            <a href="https://wp2.anakim.space/dropdown-menu/">Язык</a>
+            <ul class="sub-menu">
+                <li id="menu-item-48" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-48"><a href="https://wp2.anakim.space/contacts/">Контакты</a></li>
+            </ul>
+        </li>
+    </ul>
+</header>
+<main class="main"></main>
+<footer class="footer"></footer>
 </body>
 </html>
