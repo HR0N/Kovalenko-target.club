8921fd2620c1a6f8927a86d62fb28baa7866cd7b
Evgeny Kariuk <38400347+HR0N@users.noreply.github.com>
2022-05-22 17:09:41 +0300

    msg

--- index.php
+++ index.php
@@ -10,8 +10,9 @@
 </head>
 <body>
 <header class="header">
+    <div class="navbar-brand"><a href="/"><img src="http://placehold.jp/3d4070/ffffff/63x63.png" alt="logo"></a></div>
     <ul id="header-nav" class="header-nav">
-        <li class="navbar-brand"><a href="/"><img src="http://placehold.jp/3d4070/ffffff/63x63.png" alt="logo"></a></li>
+<!--        <li class="navbar-brand"><a href="/"><img src="http://placehold.jp/3d4070/ffffff/63x63.png" alt="logo"></a></li>-->
         <li id="menu-item-21" class="menu-item menu-item-type-custom menu-item-object-custom current-menu-item current_page_item menu-item-home menu-item-21">
             <a href="https://wp2.anakim.space/" aria-current="page">Кейсы</a>
         </li>
@@ -29,7 +30,73 @@
         </li>
     </ul>
 </header>
-<main class="main"></main>
+<main class="main">
+    <section class="section section-1">
+        <div class="filter filter-blackout"></div>
+    </section>
+    <section class="section section-2">
+        <div class="section-2__royalblue_background"></div>
+        <div class="section-2__superman"><!--<img src="./img/ek.jpg" alt="superman">--></div>
+    </section>
+    <section class="section section-3">
+        <h2>Комплексные решения для <span>Вашего бизнеса:</span></h2>
+        <div class="cards-destiny">
+            <div class="card-destiny card-1"><div class="blue_hat"></div></div>
+            <div class="card-destiny card-2"><div class="blue_hat"></div></div>
+            <div class="card-destiny card-3"><div class="blue_hat"></div></div>
+            <div class="card-destiny card-4"><div class="blue_hat"></div></div>
+            <div class="card-destiny card-5"><div class="blue_hat"></div></div>
+            <div class="card-destiny card-6"><div class="blue_hat"></div></div>
+        </div>
+    </section>
+    <section class="section section-4">
+        <h2>Этапы работы</h2>
+        <div class="work-stages">
+            <div class="work-stages__stage stage-1"><div class="work-stages__stage_stage-t-short"><span class="stage-title">Шаг</span><span class="stage-num">01</span></div><div class="work-stages__stage_stage-info"></div></div>
+            <div class="work-stages__stage stage-2"><div class="work-stages__stage_stage-t-short"><span class="stage-title">Шаг</span><span class="stage-num">02</span></div><div class="work-stages__stage_stage-info"></div></div>
+            <div class="work-stages__stage stage-3"><div class="work-stages__stage_stage-t-short"><span class="stage-title">Шаг</span><span class="stage-num">03</span></div><div class="work-stages__stage_stage-info"></div></div>
+            <div class="work-stages__stage stage-4"><div class="work-stages__stage_stage-t-short"><span class="stage-title">Шаг</span><span class="stage-num">04</span></div><div class="work-stages__stage_stage-info"></div></div>
+        </div>
+    </section>
+    <section class="section section-5">
+        <h2>Результаты сотрудничества со мной</h2>
+        <div class="cooperation-results">
+            <div class="cooperation-results__result result-1"><img src="./img/screenshot_1-1.png" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-2"><img src="./img/boots.jpg" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-3"><img src="./img/img_20161028_101938.jpg" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-4"><img src="./img/juvelirnye-ukrashenija-iz-serebra.jpg" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-5"><img src="./img/franshiza-my-coffee3.jpg" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-6"><img src="./img/american-shop.png" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-7"><img src="./img/avto_iz_usa1.jpg" alt="#"><div class="filter-blackout"></div></div>
+            <div class="cooperation-results__result result-8"><img src="./img/raskrutka-studii-lazernoi-epilyatsii.jpg" alt="#"><div class="filter-blackout"></div></div>
+        </div>
+    </section>
+    <section class="section section-6">
+        <h2>Ответы на частые вопросы</h2>
+        <p style="text-align: center; border-bottom: 2px solid royalblue; width: 150px; margin: auto">Bla-bla-bla 1, plusik</p>
+        <p style="text-align: center; border-bottom: 2px solid royalblue; width: 150px; margin: auto">Bla-bla-bla 2, plusik</p>
+        <p style="text-align: center; border-bottom: 2px solid royalblue; width: 150px; margin: auto">Bla-bla-bla 3, plusik</p>
+        <p style="text-align: center; border-bottom: 2px solid royalblue; width: 150px; margin: auto">Bla-bla-bla 4, plusik</p>
+    </section>
+    <section class="section section-7">
+        <div class="discuss-project">
+            <div class="discuss-project__form">
+                <form action="/heaven">
+                    <h2>Готовы обсудить проект?</h2>
+                    <label><input type="text" placeholder="killme"></label>
+                    <label><input type="text" placeholder="killme"></label>
+                    <label><input type="text" placeholder="killme"></label>
+                    <label><input type="text" placeholder="killme"></label>
+                </form>
+            </div>
+            <div class="discuss-project__img"><img src="./img/pc_back.jpg" alt="furniture"></div>
+        </div>
+    </section>
+    <section class="section section-8">section 8</section>
+    <section class="section section-9">section 9</section>
+    <section class="section section-10">section 10</section>
+</main>
 <footer class="footer"></footer>
+<script src="./libs/jquery.slim.min.js"></script>
 </body>
 </html>
